6e005f90c1d5e4c49481f05b14878e7365986ab4
Quang030602 <minhquang030602@gmail.com>
2025-02-27 09:34:44 +0700

    code API login : return Access Token and Refresh Token

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,24 +1,22 @@
 // Author: TrungQuanDev: https://youtube.com/@trungquandev
 import { StatusCodes } from 'http-status-codes'
 import ms from 'ms'
-
+import { JwtProvider } from '~/providers/JwtProvider'
 /**
  * Mock nhanh thông tin user thay vì phải tạo Database rồi query.
- * Nếu muốn học kỹ và chuẩn chỉnh đầy đủ hơn thì xem Playlist này nhé:
- * https://www.youtube.com/playlist?list=PLP6tw4Zpj-RIMgUPYxhLBVCpaBs94D73V
  */
 const MOCK_DATABASE = {
   USER: {
-    ID: 'trungquandev-sample-id-12345678',
-    EMAIL: 'trungquandev.official@gmail.com',
-    PASSWORD: 'trungquandev@123'
+    ID: 'aimier-sample-id-12345678',
+    EMAIL: 'minhquang030602@gmail.com',
+    PASSWORD: 'Quang030602'
   }
 }
 
 /**
  * 2 cái chữ ký bí mật quan trọng trong dự án. Dành cho JWT - Jsonwebtokens
  * Lưu ý phải lưu vào biến môi trường ENV trong thực tế cho bảo mật.
- * Ở đây mình làm Demo thôi nên mới đặt biến const và giá trị random ngẫu nhiên trong code nhé.
+ * làm Demo thôi nên mới đặt biến const và giá trị random ngẫu nhiên trong code nhé.
  * Xem thêm về biến môi trường: https://youtu.be/Vgr3MWb7aOw
  */
 const ACCESS_TOKEN_SECRET_SIGNATURE = 'KBgJwUETt4HeVD05WaXXI9V3JnwCVP'
@@ -32,6 +30,41 @@ const login = async (req, res) => {
     }
 
     // Trường hợp nhập đúng thông tin tài khoản, tạo token và trả về cho phía Client
+    const userInfo = {
+      id: MOCK_DATABASE.USER.ID,
+      email: MOCK_DATABASE.USER.EMAIL,
+    }
+    // tạo ra 2 loại token , accessToken và refreshToken
+    const accessToken = await JwtProvider.generateAccessToken(
+      userInfo, 
+      ACCESS_TOKEN_SECRET_SIGNATURE, 
+      '1h',
+    )
+    const refreshToken = await JwtProvider.generateAccessToken(
+      userInfo, 
+      REFRESH_TOKEN_SECRET_SIGNATURE , 
+      '14 days',
+    )
+    res.cookie('accessToken',accessToken,{
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+      maxAge: ms('1h')
+    })
+
+    res.cookie('refreshToken',refreshToken,{
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none', 
+      maxAge: ms('1h')
+    })
+// trả về thông tin user cũng như sẽ trả về Tokens cho trường hợp phía FE cần lưu Tokens vào stroage
+    res.status(StatusCodes.OK).json(
+      { 
+        ...userInfo, 
+        accessToken, 
+        refreshToken 
+      })
 
     res.status(StatusCodes.OK).json({ message: 'Login API success!' })
   } catch (error) {
--- src/providers/JwtProvider.js
+++ src/providers/JwtProvider.js
@@ -1,1 +1,32 @@
-// Author: TrungQuanDev: https://youtube.com/@trungquandev
+import JWT from 'jsonwebtoken'
+/*
+    function tạo mới 1 token - cần 3 tham số đầu vào 
+    userInfo: Những thông tin muốn đính kèm vào token 
+    sercretSignature: Chữ ký bí mật để tạo token
+    tokenLife: Thời gian sống của token
+
+*/ 
+const generateAccessToken = async(userInfo, sercretSignature, tokenLife) => {
+    try {
+
+        return JWT.sign(userInfo, sercretSignature, {algorithm: 'SHA256',expiresIn: tokenLife})
+    }
+    catch (error) {
+        throw new Error(error)
+    }
+}
+
+const verifyAccessToken = async(token, sercretSignature ) => {
+    try {
+        return JWT.verify(token, sercretSignature)
+    } catch (error) {
+        throw new Error(error)
+    }
+
+    
+}
+
+export const JwtProvider = {    
+    generateToken,
+    verifyToken,
+}
